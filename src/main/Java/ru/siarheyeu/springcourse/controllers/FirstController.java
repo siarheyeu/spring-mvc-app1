b142c923f76a9c97c4d074c78c3a150d44680335
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2022-12-13 23:16:05 +0300

    @RequestParam

--- src/main/Java/ru/siarheyeu/springcourse/controllers/FirstController.java
+++ src/main/Java/ru/siarheyeu/springcourse/controllers/FirstController.java
@@ -3,6 +3,7 @@ package ru.siarheyeu.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -11,9 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request){
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
+    public String helloPage(@RequestParam("name") String name, @RequestParam("surname") String surname){
 
         System.out.println("Hello, " + name + " " + surname);
 
