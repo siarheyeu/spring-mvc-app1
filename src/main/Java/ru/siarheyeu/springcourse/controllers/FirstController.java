72fd898e411c991ea5f6f94936a9e03e4d099308
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2022-12-14 17:45:46 +0300

    Data transfer from controller to view - model calculator

--- src/main/Java/ru/siarheyeu/springcourse/controllers/FirstController.java
+++ src/main/Java/ru/siarheyeu/springcourse/controllers/FirstController.java
@@ -27,4 +27,31 @@ public class FirstController {
     public String goodByePage(){
         return "first/goodbye";
     }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("a") int a, @RequestParam("b") int b,
+                             @RequestParam("action") String action, Model model){
+        double result;
+
+        switch (action){
+            case "multiplication":
+                result = a*b;
+                break;
+            case "division":
+                result = a/(double)b;
+                break;
+            case "subtraction":
+                result = a-b;
+                break;
+            case "addititon":
+                result = a+b;
+                break;
+            default:
+                result=0;
+                break;
+        }
+
+        model.addAttribute("result", result)
+        return "first/calculator";
+    }
 }
